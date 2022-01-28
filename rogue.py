ac00738768c3542556a4bcd14f5db0d7863c3db0
Clea <clea.chamfort@hotmail.fr>
2022-01-28 10:11:07 +0100

    blabla

---
+++ rogue.py
@@ -0,0 +1,19 @@
+##blabla
+import numpy as np 
+import matplotlib.pyplot as plt
+import pygame as pg
+
+# on initialise pygame et on crée une fenêtre de 400x300 pixels
+pg.init()
+screen = pg.display.set_mode((400, 300))
+# on crée aussi un objet "horloge"
+clock = pg.time.Clock()
+
+# enfin on boucle à l'infini pour faire le rendu de chaque image
+while True:
+    # l'objet "clock" permet de limiter le nombre d'images par secondes
+    # ici pour cette démo on demande 1 image par seconde
+    clock.tick(1)
+
+    # enfin on met à jour la fenêtre avec tous les changements
+    pg.display.update()
