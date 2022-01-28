62c07d17c5d8f81591a666cf5f734f8669f9c07d
MarionStenta <marion.stenta25@gmail.com>
2022-01-28 15:19:55 +0100

    J'ajoute mes couloirs

--- couloir.py
+++ couloir.py
@@ -6,27 +6,11 @@ import personnage
 class Couloir:
     def __init__(self, cases):
         self.cases = cases
-    def passage (self, x,y): #x,y est la position du personnage
-        clock = pg.time.Clock()
-        if [x,y] == self.cases[0]:
-            # Le personnage va passer automatiquement dans le couloir
-            for i in range(len(self)):
-                clock.tick(1)
-                personnage.x = self.cases[i][0]
-                personnage.y = self.cases[i][1]
-                pg.display.update()
-        elif [x,y] == self.cases[-1]:
-            # Le personnage va passer automatiquement dans le couloir
-            for i in range(len(self), -1):
-                clock.tick(1)
-                personnage.x = self.cases[i][0]
-                personnage.y = self.cases[i][1]
-                pg.display.update()
-
-    def is_in_couloir (self, x,y):
-        if [x,y] == self.cases[0]:
+    
+    def is_in_couloir (cases, x,y):
+        if [x,y] == cases[0]:
             return (True, "begin")
-        elif [x,y] == self.cases[-1]:
+        elif [x,y] == cases[-1]:
             return (True, "end")
         else: 
             return (False, "Nope")
--- rogue.py
+++ rogue.py
@@ -56,12 +56,14 @@ def draw_room(x, y, lenght, width, x_porte, y_porte):
     pg.draw.rect(screen, (139, 69, 19), rect_porte)
 
 
-def draw_couloir(couloir):
-    for i in range(len(couloir)):
-        couloir_rect = pg.Rect(couloir[i][0]*W, couloir[i][1]*H, W, H)
+def draw_couloir(corridor):
+    for i in range(len(corridor.cases)):
+        couloir_rect = pg.Rect(corridor.cases[i][0]*W, corridor.cases[i][1]*H, W, H)
         pg.draw.rect(screen, (255,127, 0), couloir_rect)
 
 perso = perso.Personnage(7,12,10,10)
+corridor = couloir.Couloir([[15,11],[16,11], [17, 11], [18,11], [19,11],[20,11]])
+
 
 # enfin on boucle à l'infini pour faire le rendu de chaque image
 while running:
@@ -69,7 +71,16 @@ while running:
     # l'objet "clock" permet de limiter le nombre d'images par secondes
     # ici pour cette démo on demande 1 image par seconde
     clock.tick(5)
-
+    bool , message = couloir.Couloir.is_in_couloir (corridor.cases,perso.x, perso.y)
+    if bool == True:
+        #print(corridor.cases)
+        if message == "begin":
+            # Le personnage va passer automatiquement dans le couloir
+            perso.x = corridor.cases[-1][0] + 1
+            perso.y = corridor.cases[-1][1]
+        elif message == "end":
+            perso.x = corridor.cases[0][0] - 1
+            perso.y = corridor.cases[0][1]
     # on itère sur tous les évènements qui ont eu lieu depuis le précédent appel
     # ici donc tous les évènements survenus durant la seconde précédente
     for event in pg.event.get():
@@ -79,17 +90,6 @@ while running:
             running = False
             break
         # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-        # bool , message = couloir.is_in_couloir (couloir, perso.x, perso.y)
-        # if bool == True:
-        #     if message == "begin":
-        #         # Le personnage va passer automatiquement dans le couloir
-        #     for i in range(len(self)):
-        #         clock.tick(1)
-        #         personnage.x = self.cases[i][0]
-        #         personnage.y = self.cases[i][1]
-        #         pg.display.update()
-        #     elif message == "end":
-
         elif event.type == pg.KEYDOWN:
             # si la touche est "Q" on veut quitter le programme
             if event.key == pg.K_q:
@@ -112,13 +112,9 @@ while running:
 
         draw_background()
         draw_tile(perso.x, perso.y, (255, 0, 0))
-    couloir = [[16,11], [17, 11], [18,11], [19,11]]
-    draw_couloir(couloir)
-
+    draw_couloir(corridor)
     draw_room(5, 7, 10, 10,15, 11)
     draw_room(20, 7, 7, 8, 20, 11)
-
-    draw_room(5, 7, 15, 12, 5, 15)
 
     # enfin on met à jour la fenêtre avec tous les changements
     pg.display.update()
