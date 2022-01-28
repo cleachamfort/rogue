fef4627794d73c5f47edb7b7809868f58f528ddb
Clea <clea.chamfort@hotmail.fr>
2022-01-28 11:28:04 +0100

    personnage mieux

--- rogue.py
+++ rogue.py
@@ -2,6 +2,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pygame as pg
+import personnage
 
 # on initialise pygame et on crée une fenêtre de 400x300 pixels
 pg.init()
@@ -10,17 +11,15 @@ screen = pg.display.set_mode((400, 300))
 clock = pg.time.Clock()
 running = True
 
+personnage = personnage(0,0,10,10,[0,1])
+
 # enfin on boucle à l'infini pour faire le rendu de chaque image
 while running:
     # l'objet "clock" permet de limiter le nombre d'images par secondes
     # ici pour cette démo on demande 1 image par seconde
     clock.tick(1)
 
-<<<<<<< HEAD
-    # on itère sur tous les évènements qui ont eu lieu depuis le précédent appel
-=======
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
->>>>>>> 4b3c54b73e796a53a7c4e1f357e0f3f0d9db03b4
     # ici donc tous les évènements survenus durant la seconde précédente
     for event in pg.event.get():
         # chaque évênement à un type qui décrit la nature de l'évênement
@@ -33,10 +32,20 @@ while running:
             # si la touche est "Q" on veut quitter le programme
             if event.key == pg.K_q:
                 running = False
+            elif event.key == pg.K_UP :
+                personnage.y = personnage.y - 1
+                personnage.direction=np.array([1,0])
+            elif event.key == pg.K_DOWN :
+                personnage.y=personnage.y - 1
+                personnage.direction=np.array([-1,0])
+                personnage.direction
+            elif event.key == pg.K_RIGHT :
+                personnage.x =personnage.x + 1 
+                personnage.direction=np.array([0,1])
+            elif event.key == pg.K_LEFT :
+                personnage.x =personnage.x + 1
+                personnage.direction=np.array([-1,0])
 
     # enfin on met à jour la fenêtre avec tous les changements
     pg.display.update()
-<<<<<<< HEAD
 pg.quit()
-=======
->>>>>>> 4b3c54b73e796a53a7c4e1f357e0f3f0d9db03b4
